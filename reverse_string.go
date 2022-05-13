a7e93e4e8c7f760d51284b507eb22d39dd156995
Sirojiddin Khujabekov <xujabekovsirojiddin@gmai.com>
2022-05-13 19:00:27 +0500

    reverse

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	n := []rune(input)
+	for i, j := 0, len(n)-1; i < j; i, j = i+1, j-1 {
+		j := len(n) - 1 - i
+		n[i], n[j] = n[j], n[i]
+		output = string(n)
+	}
 	return output
 }
